2ef67970d70872e9e3f8b0175f0c9c48e6f8af9b
Clousbi <biancamthope@gmail.com>
2025-06-08 11:00:43 -0300

    Ajuste no controle de versão - Aba Teste

--- src/back/eco_farma_API/eco_farma_API/obj/Debug/net8.0/eco_farma_API.AssemblyInfo.cs
+++ src/back/eco_farma_API/eco_farma_API/obj/Debug/net8.0/eco_farma_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eco_farma_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4efacf5e2e1cfc47dbc37d13641061b1c2293d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a925ff6cb0547b150b8aa5a2dd43fb51d1c5fcf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("eco_farma_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eco_farma_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
